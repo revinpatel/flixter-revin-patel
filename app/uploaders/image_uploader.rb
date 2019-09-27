642834a94eb80651677a4f64faf95fc3d25561ee
Revin Patel <rsp722@gmail.com>
2019-09-27 04:50:24 +0000

    Added the feature of resizing all images as they are uploaded, but it is not working.

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,7 +1,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
@@ -11,6 +11,11 @@ class ImageUploader < CarrierWave::Uploader::Base
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+
+  # Process files as they are uploaded:
+  process resize_to_fill: [800, 350]
+
+
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
