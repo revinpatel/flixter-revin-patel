c5271df42693551923fcc4e09ed44fa17ade0139
Revin Patel <rsp722@gmail.com>
2019-10-18 05:35:52 +0000

    Added authorization for enrolled students only.

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,10 +1,28 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_enrolled_for_current_course
+  
   def show
   end
-
+  
+  
   private
+  
+  def require_enrolled_for_current_course
+     if ! current_user.enrolled_in?(current_course)
+         redirect_to course_path(current_lesson.section.course), alert: 'You must be enrolled in this class.'
+     else
+         
+     end
+  end
+  
   helper_method :current_lesson
   def current_lesson
-    @current_lesson ||= Lesson.find(params[:id])
+     @current_lesson ||= Lesson.find(params[:id]) 
+  end
+  
+  helper_method :current_course
+  def current_course
+      current_lesson.section.course
   end
 end
